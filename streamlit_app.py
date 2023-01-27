0b6b433b038b4338ad17f9fe82eb56b3e058c503
Madhumalini12 <123781347+Madhumalini12@users.noreply.github.com>
2023-01-27 17:59:17 -0600

    Update streamlit_app.py
    updated spelling of avocado

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥣omega 3 & bluberry oatmeal')
 streamlit.text('🥗Kale, Spinach and Rocket smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avacado Toast')
+streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭Build Your Own Fruit Smoothie🥝🍇')
 
@@ -14,6 +14,6 @@ import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado','Strawberries'])
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 streamlit.dataframe(my_fruit_list)
