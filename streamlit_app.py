14ede025c27486f6e822abe237a672513fd395a7
Madhumalini12 <123781347+Madhumalini12@users.noreply.github.com>
2023-01-27 17:37:23 -0600

    Update streamlit_app.py
    Added Emojis and updated title and added avacado toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 
-streamlit.header('Breakfast Menu')
-streamlit.text('omega 3 & bluberry oatmeal')
-streamlit.text('Kale, Spinach and Rocket smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣omega 3 & bluberry oatmeal')
+streamlit.text('🥗Kale, Spinach and Rocket smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avacado Toast')
