2807715f039208e3744cdc6b84fb793613764ebf
Madhumalini12 <123781347+Madhumalini12@users.noreply.github.com>
2023-01-27 17:30:18 -0600

    Update streamlit_app.py
    updated header and text rows instead of all title

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 
-streamlit.title('Breakfast Menu')
-streamlit.title('omega 3 & bluberry oatmeal')
-streamlit.title('Kale, Spinach and Rocket smoothie')
-streamlit.title('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('omega 3 & bluberry oatmeal')
+streamlit.text('Kale, Spinach and Rocket smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
