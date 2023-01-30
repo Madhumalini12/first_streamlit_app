6c30ef848c7bd373c6e0a0afa2054ad1e82fba0f
Madhumalini12 <123781347+Madhumalini12@users.noreply.github.com>
2023-01-29 19:58:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ my_cur = my_cnx.cursor()
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('The user entered ', add_my_fruit)
 
-my_cur.execute("SELECT * from fruit_load_list where fruit_name="$add_my_fruit")
+my_cur.execute("SELECT * from fruit_load_list where fruit_name= '+ add_my_fruit')
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
